d42decf10a307280cfcdb89a14e1cc980e72adfe
Uvais Ahmad <uvss21@gmail.com>
2024-11-13 00:40:59 +0530

    [CREATE ] Image Upload API & make some changes in Existing CODE

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -1,42 +1,95 @@
+import { auth } from '@clerk/nextjs/server';
 import { v2 as cloudinary } from 'cloudinary';
+import { error } from 'console';
+import { NextRequest, NextResponse } from 'next/server';
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-(async function() {
+interface CloudinaryUploadResult {
+    public_id : string;
+    [key: string]: any;
+}
 
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: 'dxtbgacnf', 
-        api_key: '146734714744725', 
-        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
-    });
+export async function POST(request: NextRequest) {
+    const {userId} = auth();
+    if( !userId ){
+        return NextResponse.json({
+            error: "Unauthorized"
+        }, {status: 401});
+    }
+
+    try {
+        const formData = await request.formData();
+        const file = formData.get('file') as File | null;
+        if( !file ) {
+            return NextResponse.json({
+                error: "No file uploaded"
+            },{status: 400});
+        }
+
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes); 
+
+        const result = await new Promise<CloudinaryUploadResult>((resolve, reject) => {
+            const uploadStream = cloudinary.uploader.upload_stream(
+                { folder: 'skyCast' }, 
+                (error , result) => {
+                if(error){
+                    reject(error);
+                } else {
+                    resolve(result as CloudinaryUploadResult);
+                }
+            });
+            uploadStream.end(buffer);
+        });
+
+        return NextResponse.json({
+            public_id : result?.public_id
+        },{status: 200})
+    }
+    catch (error) {
+        return NextResponse.json({
+            error: "Uploading image failed"
+        }, {status: 500});
+    }
+}
+
+// (async function() {
+
     
-    // Upload an image
-     const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
     
-    console.log(uploadResult);
+//     // Upload an image
+//     const uploadResult = await cloudinary.uploader
+//         .upload(
+//             'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//                 public_id: 'shoes',
+//             }
+//         )
+//         .catch((error) => {
+//             console.log(error);
+//         });
     
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
+//     console.log(uploadResult);
     
-    console.log(optimizeUrl);
+//     // Optimize delivery by resizing and applying auto-format and auto-quality
+//     const optimizeUrl = cloudinary.url('shoes', {
+//         fetch_format: 'auto',
+//         quality: 'auto'
+//     });
     
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
+//     console.log(optimizeUrl);
     
-    console.log(autoCropUrl);    
-})();
+//     // Transform the image: auto-crop to square aspect_ratio
+//     const autoCropUrl = cloudinary.url('shoes', {
+//         crop: 'auto',
+//         gravity: 'auto',
+//         width: 500,
+//         height: 500,
+//     });
+    
+//     console.log(autoCropUrl);    
+// })();
